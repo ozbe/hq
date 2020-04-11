3ebc1663f994e1ef334f543a8972c52deceba676
Josh Aaseby <1372945+ozbe@users.noreply.github.com>
2020-04-11 11:25:09 -0400

    Add optional file argument (#2)
    * add optional file argument
    
    * apply rust fmt

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,55 @@
 use scraper::{Html, Selector};
 use std::io::{self, BufRead};
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Args {
     selectors: String,
+    #[structopt(parse(from_os_str))]
+    file: Option<PathBuf>,
 }
 
 fn main() {
     let args = Args::from_args();
-    let selector = Selector::parse(&args.selectors).unwrap();
 
-    let content: String = {
-        let stdin = io::stdin();
-        stdin
-            .lock()
-            .lines()
-            .filter_map(|l| l.ok())
-            .collect::<Vec<String>>()
-            .join("")
-    };
+    let selector = get_selector(&args.selectors);
+    let html = get_html(&args.file);
+    extract(&selector, html)
+}
 
-    let document = Html::parse_fragment(&content);
-    for element in document.select(&selector) {
+fn get_selector(selectors: &str) -> Selector {
+    Selector::parse(selectors).unwrap()
+}
+
+fn get_html(file: &Option<PathBuf>) -> Html {
+    let fragment = get_content(file);
+    Html::parse_fragment(&fragment)
+}
+
+fn get_content(file: &Option<PathBuf>) -> String {
+    match file {
+        Some(ref file) => read_from_file(file),
+        None => read_from_stdin(),
+    }
+}
+
+fn read_from_file(path: &PathBuf) -> String {
+    std::fs::read_to_string(path).unwrap()
+}
+
+fn read_from_stdin() -> String {
+    let stdin = io::stdin();
+    stdin
+        .lock()
+        .lines()
+        .filter_map(|l| l.ok())
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+fn extract(selector: &Selector, html: Html) {
+    for element in html.select(&selector) {
         println!("{:?}", element.html());
     }
 }
