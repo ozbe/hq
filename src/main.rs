8ce6021926eb2dc44dc774344d569d9de50cadf1
Josh Aaseby <1372945+ozbe@users.noreply.github.com>
2020-05-16 12:57:23 -0400

    Output (more) human friendly errors (#8)
    * convert unwraps to failure::Error and output errors on exit
    
    * replace failure & exitfailure with anyhow
    
    * rename ParseError to SelectorParseError

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+use anyhow::{Context, Result};
+use cssparser::{BasicParseErrorKind, ParseErrorKind};
 use scraper::{Html, Selector};
+use selectors::parser::SelectorParseErrorKind;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 use std::io::{self, BufRead};
 use std::path::PathBuf;
 use structopt::StructOpt;
@@ -21,32 +26,39 @@ struct Args {
     file: Option<PathBuf>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::from_args();
 
-    let selector = get_selector(&args.selectors);
-    let html = get_html(&args.file);
-    extract(&selector, html)
+    let selector = get_selector(&args.selectors)?;
+    let html = get_html(&args.file)?;
+    extract(&selector, html);
+
+    Ok(())
 }
 
-fn get_selector(selectors: &str) -> Selector {
-    Selector::parse(selectors).unwrap()
+fn get_selector(selectors: &str) -> Result<Selector> {
+    Selector::parse(selectors)
+        .map_err(SelectorParseError::from)
+        .with_context(|| format!("could not parse selectors `{}`", selectors))
+        .map_err(From::from)
 }
 
-fn get_html(file: &Option<PathBuf>) -> Html {
-    let fragment = get_content(file);
-    Html::parse_fragment(&fragment)
+fn get_html(file: &Option<PathBuf>) -> Result<Html> {
+    let fragment = get_content(file)?;
+    Ok(Html::parse_fragment(&fragment))
 }
 
-fn get_content(file: &Option<PathBuf>) -> String {
+fn get_content(file: &Option<PathBuf>) -> Result<String> {
     match file {
         Some(ref file) => read_from_file(file),
-        None => read_from_stdin(),
+        None => Ok(read_from_stdin()),
     }
 }
 
-fn read_from_file(path: &PathBuf) -> String {
-    std::fs::read_to_string(path).unwrap()
+fn read_from_file(path: &PathBuf) -> Result<String> {
+    std::fs::read_to_string(path)
+        .with_context(|| format!("could not read file `{}`", path.as_path().display()))
+        .map_err(From::from)
 }
 
 fn read_from_stdin() -> String {
@@ -64,3 +76,86 @@ fn extract(selector: &Selector, html: Html) {
         println!("{:?}", element.html());
     }
 }
+
+#[derive(Debug)]
+struct SelectorParseError(String);
+
+impl Display for SelectorParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<'i> From<cssparser::ParseError<'i, SelectorParseErrorKind<'i>>> for SelectorParseError {
+    fn from(e: cssparser::ParseError<'i, SelectorParseErrorKind<'i>>) -> Self {
+        let msg = match e.kind {
+            ParseErrorKind::Basic(b) => match b {
+                BasicParseErrorKind::UnexpectedToken(_) => "unexpected token".to_string(),
+                BasicParseErrorKind::EndOfInput => {
+                    "end of the input was encountered unexpectedly.".to_string()
+                }
+                BasicParseErrorKind::AtRuleInvalid(a) => format!("invalid `@` rule {}", a),
+                BasicParseErrorKind::AtRuleBodyInvalid => {
+                    "body of an '@' rule was invalid".to_string()
+                }
+                BasicParseErrorKind::QualifiedRuleInvalid => {
+                    "qualified rule was encountered that was invalid".to_string()
+                }
+            },
+            ParseErrorKind::Custom(kind) => match kind {
+                SelectorParseErrorKind::BadValueInAttr(_) => "bad value in attribute".to_string(),
+                SelectorParseErrorKind::PseudoElementInComplexSelector => {
+                    "pseudo element is complex selector".to_string()
+                }
+                SelectorParseErrorKind::NoQualifiedNameInAttributeSelector(_) => {
+                    "no qualified name in attribute selector".to_string()
+                }
+                SelectorParseErrorKind::EmptySelector => "empty selector".to_string(),
+                SelectorParseErrorKind::DanglingCombinator => "dangling combinator".to_string(),
+                SelectorParseErrorKind::NonSimpleSelectorInNegation => {
+                    "non simple selector in negation".to_string()
+                }
+                SelectorParseErrorKind::NonCompoundSelector => "non compound selector".to_string(),
+                SelectorParseErrorKind::NonPseudoElementAfterSlotted => {
+                    "non pseudo element after slotted".to_string()
+                }
+                SelectorParseErrorKind::InvalidPseudoElementAfterSlotted => {
+                    "invalid pseudo element after slotted".to_string()
+                }
+                SelectorParseErrorKind::UnexpectedTokenInAttributeSelector(_) => {
+                    "unexpected token in attribute selector".to_string()
+                }
+                SelectorParseErrorKind::PseudoElementExpectedColon(_) => {
+                    "pseudo element expected colon".to_string()
+                }
+                SelectorParseErrorKind::PseudoElementExpectedIdent(_) => {
+                    "pseudo element expected identity".to_string()
+                }
+                SelectorParseErrorKind::NoIdentForPseudo(_) => "no identity for pseudo".to_string(),
+                SelectorParseErrorKind::UnsupportedPseudoClassOrElement(s) => {
+                    format!("unsupported pseudo class or element `{}`", s)
+                }
+                SelectorParseErrorKind::UnexpectedIdent(s) => {
+                    format!("unexpected identity `{}`", s)
+                }
+                SelectorParseErrorKind::ExpectedNamespace(s) => {
+                    format!("expected namespace `{}`", s)
+                }
+                SelectorParseErrorKind::ExpectedBarInAttr(_) => {
+                    "expected bar in attribute".to_string()
+                }
+                SelectorParseErrorKind::InvalidQualNameInAttr(_) => {
+                    "invalid qualified name in attribute".to_string()
+                }
+                SelectorParseErrorKind::ExplicitNamespaceUnexpectedToken(_) => {
+                    "explicit namespace unexpected token".to_string()
+                }
+                SelectorParseErrorKind::ClassNeedsIdent(_) => "class needs identity".to_string(),
+                SelectorParseErrorKind::EmptyNegation => "empty negation".to_string(),
+            },
+        };
+        SelectorParseError(msg)
+    }
+}
+
+impl Error for SelectorParseError {}
